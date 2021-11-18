11c2be822e9f88aef4f24aabe637c20f65a60753
jonahtabb <jtabbdev@gmail.com>
2021-11-17 19:22:20 -0500

    Added basic scafolding for HomePage and HomeBar bar components
    The home page bar was added to match the exsting set up, but allow for a different styling than the logged out bar, as defined in the mocks

---
+++ client/src/pages/Homepage/useStyles.ts
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {
+    minHeight: '100vh',
+  },
+  homePageWrapper: {
+    marginTop: '300px',
+  },
+}));
+
+export default useStyles;
