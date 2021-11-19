dde12e18fd176b738d93d9baee985b5b27203484
jonahtabb <jtabbdev@gmail.com>
2021-11-19 09:24:12 -0500

    Base home page styling

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -1,9 +1,16 @@
 import useStyles from './useStyles';
+import { Box, Grid } from '@material-ui/core';
+import homepage_image from '../../Images/homepage_image_v2.png';
 export default function Homepage(): JSX.Element {
   const classes = useStyles();
   return (
-    <div className={classes.homePageWrapper}>
-      <h1>This is the Home Page</h1>
-    </div>
+    <Box className={classes.wrapper}>
+      <Grid container className={classes.gridContainer}>
+        <Grid item xs={6} className={classes.contentWrapper}></Grid>
+        <Grid item xs={6} className={classes.imageWrapper}>
+          <img className={classes.image} src={homepage_image} alt="" width="100%" />
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
