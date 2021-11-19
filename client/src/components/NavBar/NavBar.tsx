a4be145de57b6e24fd4c25a29516e207e89d61d3
jonahtabb <jtabbdev@gmail.com>
2021-11-19 09:44:43 -0500

    Updated default navbar styles
    Added a useEffect that conditinal displays thte different styles for the NavBar depending on pathname

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -4,7 +4,8 @@ import useStyles from './useStyles';
 import logo from '../../Images/logo.png';
 import { useAuth } from '../../context/useAuthContext';
 import { useSocket } from '../../context/useSocketContext';
-import { useEffect } from 'react';
+import { useHistory, useLocation } from 'react-router-dom';
+import { useEffect, useRef } from 'react';
 import LoggedInBar from './AuthBars/LoggedInBar';
 import LoggedOutBar from './AuthBars/LoggedOutBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
@@ -15,10 +16,28 @@ const NavBar = (): JSX.Element => {
 
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
+  const outterNavStyle = useRef(classes.appbarHomePage);
+  const innerNavStyle = useRef(classes.appbarHomePage);
+
+  const history = useHistory();
+  const location = useLocation();
 
   useEffect(() => {
+    console.log('**************');
     initSocket();
   }, [initSocket]);
+
+  useEffect(() => {
+    console.log('**************');
+    const path = history.location.pathname;
+    if (path === '/') {
+      outterNavStyle.current = classes.appbarHomePage;
+      innerNavStyle.current = classes.homePageBar;
+    } else {
+      outterNavStyle.current = classes.appbar;
+      innerNavStyle.current = classes.toolbar;
+    }
+  }, [history, classes, location]);
 
   if (loggedInUser === undefined) return <CircularProgress />;
   if (
@@ -33,9 +52,9 @@ const NavBar = (): JSX.Element => {
   }
 
   return (
-    <AppBar className={history.location.pathname === '/' ? classes.appbarHomePage : classes.appbar} position="absolute">
+    <AppBar className={outterNavStyle.current} position="absolute">
       <CssBaseline />
-      <ToolBar className={history.location.pathname === '/' ? classes.homePageBar : classes.toolbar}>
+      <ToolBar className={innerNavStyle.current}>
         <img src={logo} alt="logo" />
         {loggedInUser ? <LoggedInBar /> : history.location.pathname === '/' ? <HomePageBar /> : <LoggedOutBar />}
       </ToolBar>
