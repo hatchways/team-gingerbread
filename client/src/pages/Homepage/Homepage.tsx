842f4f31d8be46fa19564087298fd47b156c9ea2
jonahtabb <jtabbdev@gmail.com>
2021-11-19 11:14:00 -0500

    home page styles updated

--- client/src/pages/Homepage/Homepage.tsx
+++ client/src/pages/Homepage/Homepage.tsx
@@ -1,13 +1,43 @@
 import useStyles from './useStyles';
-import { Box, Grid } from '@material-ui/core';
+import { Box, Grid, TextField, InputLabel, Button } from '@material-ui/core';
 import homepage_image from '../../Images/homepage_image_v2.png';
 export default function Homepage(): JSX.Element {
   const classes = useStyles();
   return (
     <Box className={classes.wrapper}>
       <Grid container className={classes.gridContainer}>
-        <Grid item xs={6} className={classes.contentWrapper}></Grid>
-        <Grid item xs={6} className={classes.imageWrapper}>
+        <Grid item xs={12} sm={6} className={classes.contentWrapper}>
+          <form className={classes.form}>
+            <h1>Find the care your dog deserves</h1>
+            <InputLabel shrink htmlFor="location">
+              Where
+            </InputLabel>
+            <TextField id="location" placeholder="Anywhere" variant="outlined" />
+            <InputLabel shrink htmlFor="dropin-date">
+              Drop in / Drop off
+            </InputLabel>
+            <Box>
+              <TextField
+                id="dropin-date"
+                type="date"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+              <TextField
+                id="dropofff-date"
+                type="date"
+                variant="outlined"
+                InputLabelProps={{
+                  shrink: true,
+                }}
+              />
+            </Box>
+            <Button variant="contained">Find My Dog Sitter</Button>
+          </form>
+        </Grid>
+        <Grid item xs={12} sm={6} className={classes.imageWrapper}>
           <img className={classes.image} src={homepage_image} alt="" width="100%" />
         </Grid>
       </Grid>
--- client/src/pages/Homepage/useStyles.ts
+++ client/src/pages/Homepage/useStyles.ts
@@ -12,7 +12,43 @@ const useStyles = makeStyles(() => ({
   gridContainer: {
     height: '100vh',
   },
-  contentWrapper: {},
+  contentWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    padding: '15px',
+    maxWidth: '500px',
+    margin: 'auto',
+    '& h1': {
+      fontSize: '2.5rem',
+    },
+    '& label': {
+      color: 'black',
+      fontWeight: '900',
+      textTransform: 'uppercase',
+      marginTop: '20px',
+      marginBottom: '5px',
+    },
+    '& button': {
+      display: 'flex',
+      color: 'white',
+      fontWeight: 'normal',
+      backgroundColor: '#F14140',
+      boxShadow: 'none',
+      padding: '10px 30px 10px 30px',
+      margin: '20px auto 0px 0px',
+      '&:hover': {
+        backgroundColor: 'white',
+        color: '#F14140',
+        border: '1px solid #F14140',
+        boxShadow: 'none',
+      },
+    },
+  },
   imageWrapper: {
     overflow: 'hidden',
     width: '100%',
