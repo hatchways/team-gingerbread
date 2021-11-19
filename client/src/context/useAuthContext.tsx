2a5cdb77911191d4c74cc25fad478815574d1375
jonahtabb <jtabbdev@gmail.com>
2021-11-19 09:23:42 -0500

    updated on page load routing
    When user is not logged in routing now defaults to homepage instead of login

--- client/src/context/useAuthContext.tsx
+++ client/src/context/useAuthContext.tsx
@@ -54,7 +54,7 @@ export const AuthProvider: FunctionComponent = ({ children }): JSX.Element => {
           setLoggedInUser(null);
         } else {
           // don't need to provide error feedback as this just means user doesn't have saved cookies or the cookies have not been authenticated on the backend
-          history.push('/login');
+          history.push('/');
           setLoggedInUser(null);
         }
       });
