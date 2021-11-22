af6ac79ac52449be30a5abfc5a8db486ef3b0e6f
jonahtabb <jtabbdev@gmail.com>
2021-11-22 17:29:28 -0500

    debugged conditional styling of Nav

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -9,15 +9,30 @@ import LoggedInBar from './AuthBars/LoggedInBar';
 import LoggedOutBar from './AuthBars/LoggedOutBar';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import HomePageBar from './AuthBars/HomePageBar';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const NavBar = (): JSX.Element => {
   const classes = useStyles();
   const history = useHistory();
+  const location = useLocation();
   const { loggedInUser } = useAuth();
   const { initSocket } = useSocket();
   const outterNavStyle = useRef(classes.appbarHomePage);
   const innerNavStyle = useRef(classes.appbarHomePage);
+
+  useEffect(() => {
+    const locationChange = history.listen((location) => {
+      if (location.pathname === '/') {
+        outterNavStyle.current = classes.appbarHomePage;
+        innerNavStyle.current = classes.homePageBar;
+      } else {
+        outterNavStyle.current = classes.appbar;
+        innerNavStyle.current = classes.toolbar;
+      }
+    });
+
+    return locationChange;
+  }, [history, classes, location]);
 
   useEffect(() => {
     initSocket();
