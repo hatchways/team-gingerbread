737c1ab926db33496be7587b811df34822b27195
jonahtabb <jtabbdev@gmail.com>
2021-11-22 20:01:43 -0500

    basic functional set up for route /bookingrequest/create

--- server/controllers/bookingRequest.js
+++ server/controllers/bookingRequest.js
@@ -1,11 +1,36 @@
-const BookingRequest = require("../models/BookingRequest")
-const asyncHandler = require("express-async-handler")
+const User = require("../models/User");
+const BookingRequest = require("../models/BookingRequest");
+const asyncHandler = require("express-async-handler");
 
-// @route POST /bookingrequests
+// @route POST /bookingrequests/create
 // @desc Create a new request
 // @access Private
 
 exports.bookRequest = asyncHandler(async (req, res, next) => {
-    const user_id = req.user.id
-    console.log(user_id)
+
+    try {
+        const userId = req.user.id
+        const user = await User.findById(userId)
+        const {sitter_id, start, end, description} = req.body
+        if (user) {
+            const sitter = await User.findById(sitter_id)
+            if (sitter){
+                const bookingRequest = await BookingRequest.create({
+                    user_id: user,
+                    sitter_id: sitter,
+                    start,
+                    end,
+                    description
+                })
+                console.log(bookingRequest)
+            }
+            else throw new Error("Unable to find the dog sitter info")
+        } else throw new Error("Unable to find the dog owner info")
+        // console.log(user)
+
+    } catch (error){
+        console.error(error)
+        return error
+    }
+
 })
