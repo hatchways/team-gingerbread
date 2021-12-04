e7580064259cd81a3e7bd3c389198c1482f2e59d
jb162 <78042694+jb162@users.noreply.github.com>
2021-12-03 17:22:22 -0800

    removed hard-coded userId
    removed hard-coded userId (will replace on backend with req.user.id via protect middleware)

--- client/src/helpers/APICalls/getAllUnreadNotifications.ts
+++ client/src/helpers/APICalls/getAllUnreadNotifications.ts
@@ -1,9 +1,7 @@
 import { NotificationApiData } from '../../interface/NotificationApiData';
 
 const getUnread = async (): Promise<NotificationApiData> => {
-  const userId = '619c1eb37a1e963a5b179c4b';
-
-  return await fetch(`/notifications/unread/${userId}`)
+  return await fetch(`/notifications/unread`)
     .then((res) => res.json())
     .catch(() => ({
       error: { message: 'Unable to connect to server. Please try again' },
