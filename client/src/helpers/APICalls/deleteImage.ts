9056963101760ec6f5bf57bd4e47e112daf97a97
Ankit Babber <ankitbabber@gmail.com>
2021-12-07 18:04:55 -0600

    refactored deleteImage controller to use post method, refactored interafaces for delete and upload image api calls, added protected route middleware to image route

--- client/src/helpers/APICalls/deleteImage.ts
+++ client/src/helpers/APICalls/deleteImage.ts
@@ -1,22 +1,15 @@
-interface DeleteImageDataSuccess {
-  message: string;
-}
+import { FetchOptions } from '../../interface/FetchOptions';
+import { DeleteImageApiData } from '../../interface/DeleteImageApiData';
 
-interface DeleteImageData {
-  error?: { message: string };
-  succss: DeleteImageDataSuccess;
-}
-
-interface DeleteFetchOptions {
-  method: string;
-}
-
-const deleteImage = async (_id: string): Promise<DeleteImageData> => {
-  const fetchOptions: DeleteFetchOptions = {
-    method: 'DELETE',
+const deleteImage = async (_id: string): Promise<DeleteImageApiData> => {
+  const fetchOptions: FetchOptions = {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ _id }),
+    credentials: 'include',
   };
 
-  return await fetch(`/image/delete?` + new URLSearchParams({ _id }), fetchOptions)
+  return await fetch('/image/delete', fetchOptions)
     .then((res) => res.json())
     .catch((e) => ({
       error: { message: `Unable to connect to server. Please try again ${e}` },
--- client/src/helpers/APICalls/uploadImage.ts
+++ client/src/helpers/APICalls/uploadImage.ts
@@ -1,18 +1,7 @@
-interface UploadImageDataSuccess {
-  message: string;
-}
+import { UploadFetchOptions } from '../../interface/UploadFetchOptions';
+import { UploadImageApiData } from '../../interface/UploadImageApiData';
 
-interface UploadImageData {
-  error?: { message: string };
-  succss: UploadImageDataSuccess;
-}
-
-interface UploadFetchOptions {
-  method: string;
-  body?: FormData;
-}
-
-const uploadImage = async (images: Array<File>, _id: string): Promise<UploadImageData> => {
+const uploadImage = async (images: Array<File>, _id: string): Promise<UploadImageApiData> => {
   const formData = new FormData();
   formData.append('images', images[0]);
   formData.append('_id', _id);
@@ -20,6 +9,7 @@ const uploadImage = async (images: Array<File>, _id: string): Promise<UploadImag
   const fetchOptions: UploadFetchOptions = {
     method: 'POST',
     body: formData,
+    credentials: 'include',
   };
   return await fetch(`/image/upload`, fetchOptions)
     .then((res) => res.json())
