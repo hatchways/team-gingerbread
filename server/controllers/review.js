5cfa5235a857db8c8fd3ca3725138d09c9db67c9
Justin Baytosh <justin.baytosh@gmail.com>
2022-01-10 15:44:29 -0800

    Added validation
    still need to test validation

--- server/controllers/review.js
+++ server/controllers/review.js
@@ -2,20 +2,45 @@ const Review = require("../models/Review");
 
 // @route GET /reviews/all/:sitterId
 // @desc gets all reviews for sitter
-// @access Public
+// @access Private
 exports.getAllReviews = async (req, res) => {
   const reviews = await Review.find({ sitterId: req.params.sitterId });
   res.json({ success: reviews });
 };
 
-// @route POST /reviews/create/:sitterId
+// @route POST /reviews/create
 // @desc creates new review
-// @access Public
+// @access Private
 exports.createReview = async (req, res) => {
-  const { clientId, rating, description } = req.body;
+  const { sitterId, rating, description } = req.body;
+
+  const sitter = await User.findById(sitterId);
+  const sitterProfile = await Profile.findById(sitter.profile);
+
+  if (!sitterProfile.isSitter) {
+    res.json({ error: "Supplied sitter is not a sitter (this should be true)" });
+  }
+
+  res.send({ success: req.user.id });
+
+  const client = await User.findById(req.user.id);
+  const clientProfile = await Profile.findById(client.profile);
+
+  if (clientProfile.isSitter) {
+    res.json({ error: "Supplied client is a sitter (this should be false)" });
+  }
+
+  if (!(rating >= 1 && rating <= 5)) {
+    res.json({ error: "Rating is out-of-bounds (1 to 5)" });
+  }
+
+  if (typeof description !== "string") {
+    res.json({ error: "Description is not a string" });
+  }
+
   const review = new Review({
-    sitterId: req.params.sitterId,
-    clientId,
+    sitterId,
+    clientId: req.user.id,
     rating,
     description,
   });
--- server/routes/review.js
+++ server/routes/review.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const { getAllReviews, createReview } = require("../controllers/review");
+const protect = require("../middleware/auth");
 
 const router = express.Router();
 
-router.route("/all/:sitterId").get(getAllReviews);
-router.route("/create/:sitterId").post(createReview);
+router.route("/all/:sitterId").get(protect, getAllReviews);
+router.route("/create").post(protect, createReview);
 
 module.exports = router;
